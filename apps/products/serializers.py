75cec4e7a528eeb9eace8312d7459d72b61d6a05
Hyerim Choi <hyerimc858@gmail.com>
2021-12-03 15:12:05 +0900

    clean push

--- apps/products/serializers.py
+++ apps/products/serializers.py
@@ -59,3 +59,27 @@ class CreateUpdateMenuSerialier(serializers.ModelSerializer):
         for tag_data in tags_data:
             Tag.objects.create(menuId=menu, **tag_data)
         return menu
+    
+    def update(self, instance, validated_data):
+        items_data = validated_data.pop("items")
+        tags_data = validated_data.pop("tags")
+        
+        # Maps for id->instance and id->data item.
+        book_mapping = {book.id: book for book in instance}
+        data_mapping = {item['id']: item for item in validated_data}
+
+        # Perform creations and updates.
+        ret = []
+        for book_id, data in data_mapping.items():
+            book = book_mapping.get(book_id, None)
+            if book is None:
+                ret.append(self.child.create(data))
+            else:
+                ret.append(self.child.update(book, data))
+
+        # Perform deletions.
+        for book_id, book in book_mapping.items():
+            if book_id not in data_mapping:
+                book.delete()
+
+        return ret
